1b344f7dae21a8a3e877b3ceb0da393cc7988886
FHC <fcardoso.poa@gmail.com>
2024-05-15 23:07:49 -0300

    configuring posts and routes login/logout

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +13,9 @@ import { PrimaryInputComponent } from '../../components/primary-input/primary-in
     ReactiveFormsModule,
     PrimaryInputComponent
   ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -18,10 +23,25 @@ export class LoginComponent {
 
   loginForm!: FormGroup
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private loginService: LoginService
+  ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
+  }
+
+  submit() {
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => console.log("sucesso"),
+      error: () => console.log("error")
+    })
+  }
+
+  navigate() {
+    //console.log(this.loginForm.value)
+    this.router.navigate(["signup"])
   }
 }
